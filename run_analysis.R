47c98c9d1d10e4376e6ff14c9d85eca44a084a9f
ThomasZhang <18712728771@163.com>
2017-12-10 11:16:10 +0800

    fix

--- run_analysis.R
+++ run_analysis.R
@@ -26,20 +26,20 @@ names(merged_data) <- c(features$V2, "subject", "labels")
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 
-mean_std_data <- merged_data[, grep("[Mm]ean|std", names(merged_data), value = TRUE)]
+mean_std_data <- merged_data[, grep("[Mm]ean|std|subject|labels", names(merged_data), value = TRUE)]
 
 # 3. Uses descriptive activity names to name the activities in the data set
 
-colnames(merged_data)[563] <- 'activity'
+colnames(mean_std_data)[88] <- 'activity'
 activity_labels <- fread("./data/UCI HAR Dataset/activity_labels.txt")
-merged_data$activity <- activity_labels$V2[merged_data$activity]
+mean_std_data$activity <- activity_labels$V2[mean_std_data$activity]
 
 # 4. Appropriately labels the data set with descriptive variable names.
 
-names(merged_data) <- tolower(names(merged_data))
-names(merged_data) <- gsub("_|,", "-", names(merged_data))
+names(mean_std_data) <- tolower(names(mean_std_data))
+names(mean_std_data) <- gsub("_|,", "-", names(mean_std_data))
 
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-average_data <- melt(merged_data, id=c("subject", "activity")) %>% dcast(subject + activity ~ variable, mean)
+average_data <- melt(mean_std_data, id=c("subject", "activity")) %>% dcast(subject + activity ~ variable, mean)
 write.table(average_data, file = "./data/UCI HAR Dataset/tidy data.txt", row.name=FALSE)
